744ec5befc491859abd3ce9214ea71307d1ff7a7
Marco Cambon <marco.cambon@elmec.com>
2020-04-01 15:20:16 +0200

    log timezone

--- app/logger/index.js
+++ app/logger/index.js
@@ -1,26 +1,34 @@
 var winston = require('winston');
+const format = require('winston');
+const moment = require('moment-timezone');
 var httpContext = require('express-http-context');
 // Wrap Winston logger to print reqId in each log
 var reqId = function() {
   return httpContext.get('reqId');
 };
 
+const appendTimestamp = winston.format((info, opts) => {
+  if(opts.tz)
+    info.timestamp = moment().tz(opts.tz).format('DD-MM-YYYY hh:mm:ss:SSS').trim();
+  return info;
+});
 
 //const deflogger = winston.createLogger({
 winston.configure({  
   level: 'debug',
   format: winston.format.combine(
-    winston.format.timestamp(),
+    appendTimestamp({ tz: 'Europe/Rome' }),
+    //winston.format.timestamp(),
     winston.format.printf(log => {
       rId = reqId();
-      msg = rId ? `${rId} | ${log.timestamp} | ${log.level}: ${log.message}` :  `${log.timestamp} | ${log.level}: ${log.message}`;
+      msg = rId ? `${rId} | ${log.timestamp} | [${log.level}]: ${log.message}` :  `${log.timestamp} | [${log.level}]: ${log.message}`;
       return msg;
     })
   ),
   //defaultMeta: { service: 'user-service' },
   transports: [
-    new winston.transports.File({ filename: '../logs/error.log', level: 'error' }),
-    new winston.transports.File({ filename: '../logs/combined.log' })
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' })
   ]
 });
 // If we're not in production then log also to the `console` 
